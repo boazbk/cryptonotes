771e7213ebe257751590357c3788c524b4b5ba4c
Boaz Barak <b@boazbarak.org>
2019-12-30 01:45:37 -0500

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,7 @@ git pull --rebase
 git push
 cd ..
 echo "Deploying binaries to s3"
-cp -Rf latex-book/lnotes_book.pdf binaries/
+cp -Rf latex-book/*.pdf binaries/
 cp -Rf binaries/* C:/DBOX/WORK/Homepage/Binary/crypto/
 cd "C:/DBOX/WORK/Homepage/Binary/crypto"
 read -p "Run acrobat? " -n 1 -r
